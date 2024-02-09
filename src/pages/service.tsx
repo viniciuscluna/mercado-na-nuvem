114df102cf2e5fb08826108206769d43f794c3b2
Vinicius Luna <viniciuscluna@gmail.com>
2024-02-08 21:03:22 -0300

    feat: clear service page

--- src/pages/service.tsx
+++ src/pages/service.tsx
@@ -12,19 +12,19 @@ const Service = () => {
     <>
       <div className="flex justify-between items-center">
         <h2 className="text-3xl font-extrabold dark:text-white my-6">
-          Prestação de Serviços
+         Venda de Produtos
         </h2>
-        <button
+        {/* <button
           type="button"
           onClick={() => changeIsOpened()}
           className="text-white  bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
         >
           Adicionar Serviço
-        </button>
+        </button> */}
       </div>
-      <Board />
+      {/* <Board />
       <Include />
-      <Update />
+      <Update /> */}
     </>
   );
 };
