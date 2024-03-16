06aa657c204dc0b3ab27918f777d8615eb69836a
Vinicius Luna <viniciuscluna@gmail.com>
2024-03-15 21:51:25 -0300

    feat: include peso

--- src/components/service/sell/form.tsx
+++ src/components/service/sell/form.tsx
@@ -64,6 +64,7 @@ const Form = ({ control, addProduct, port }: FormProps) => {
                                 // Iterando sobre os valores encontrados
                                 matches.forEach(match => {
                                     setValue('peso', Number(match));
+                                    console.log('peso', Number(match))
                                     // Faça algo com cada valor encontrado
                                 });
                             }
@@ -115,6 +116,10 @@ const Form = ({ control, addProduct, port }: FormProps) => {
                 <div>
                     <label htmlFor="total" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Total</label>
                     <input {...register('total')} type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                </div>
+                <div>
+                    <label htmlFor="peso" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Peso</label>
+                    <input {...register('peso')} type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
                 </div>
                 <div>
                     <button type="submit" className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Incluir Produto</button>
