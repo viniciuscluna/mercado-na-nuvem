ddde551dd5aee436eaba676141179ac15581dea2
Vinicius Luna <viniciuscluna@gmail.com>
2024-03-27 21:46:40 -0300

    fix: update ordem venda endpoint

--- src/services/ordemVendaService.ts
+++ src/services/ordemVendaService.ts
@@ -2,6 +2,6 @@ import { OrdemVenda } from "../domain/ordemVenda";
 import { instanceApi } from "./axiosConfig";
 
 export const add = async (ordemVenda: OrdemVenda): Promise<OrdemVenda> => {
-  return (await instanceApi.post<OrdemVenda>("/produto", ordemVenda)).data;
+  return (await instanceApi.post<OrdemVenda>("/ordemVenda", ordemVenda)).data;
 };
 
